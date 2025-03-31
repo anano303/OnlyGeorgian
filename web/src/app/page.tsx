4b02aa83cb5131a8e300a294da5d65b5066c3b38
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-01 01:24:03 +0400

    .

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,7 @@ import HomePageShop from "@/components/homePageShop/homePageShop";
 import LiveChat from "@/components/liveChat/liveChat";
 import TopItems from "@/components/TopItems/TopItems";
 import FeaturedProducts from "@/components/FeaturedProducts/FeaturedProducts";
-import LiveChat from "../components/liveChat/liveChat";
+
 
 const Home = () => {
   return (
