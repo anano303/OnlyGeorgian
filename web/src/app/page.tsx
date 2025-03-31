83ba20bccccf1fec273d1e51e9e1525d05174c95
teokartvelishvili <qartvelishviliteona@gmail.com>
2025-03-31 13:05:46 -0800

    Update page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,15 +1,18 @@
-// import HomePageForum from "@/components/homePageForum/homePageForum";
 import Description from "@/components/descriptions/description";
 import HomePagesHead from "@/components/homePagesHead/homePagesHead";
 import HomePageShop from "@/components/homePageShop/homePageShop";
+<<<<<<< HEAD
 import LiveChat from "@/components/liveChat/liveChat";
 // import Navbar from "@/components/navbar/navbar";
+=======
+>>>>>>> 501e4a959caa1dfec8ff5a34b4e8e97be2d9d1ca
 import TopItems from "@/components/TopItems/TopItems";
-const Home = () => {
-  
+import FeaturedProducts from "@/components/FeaturedProducts/FeaturedProducts";
 
+const Home = () => {
   return (
     <div>
+<<<<<<< HEAD
      <LiveChat/>
      <HomePagesHead/>
       <TopItems/>
@@ -17,6 +20,13 @@ const Home = () => {
       <HomePageShop/>
       {/* <HomePageForum/> */}
       <Description/>
+=======
+      <HomePagesHead />
+      <TopItems />
+      <HomePageShop />
+      <Description />
+      <FeaturedProducts />
+>>>>>>> 501e4a959caa1dfec8ff5a34b4e8e97be2d9d1ca
     </div>
   );
 };
