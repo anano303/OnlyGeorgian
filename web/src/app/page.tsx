e6257837b6eda4a3c480a8effa6ad4674ee51cdf
anano303 <124179773+anano303@users.noreply.github.com>
2025-04-01 00:56:50 +0400

    .

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,20 +1,17 @@
-// import HomePageForum from "@/components/homePageForum/homePageForum";
 import Description from "@/components/descriptions/description";
 import HomePagesHead from "@/components/homePagesHead/homePagesHead";
 import HomePageShop from "@/components/homePageShop/homePageShop";
-// import Navbar from "@/components/navbar/navbar";
 import TopItems from "@/components/TopItems/TopItems";
-const Home = () => {
-  
+import FeaturedProducts from "@/components/FeaturedProducts/FeaturedProducts";
 
+const Home = () => {
   return (
     <div>
-     <HomePagesHead/>
-      <TopItems/>
-      {/* <Navbar/> */}
-      <HomePageShop/>
-      {/* <HomePageForum/> */}
-      <Description/>
+      <HomePagesHead />
+      <TopItems />
+      <HomePageShop />
+      <Description />
+      <FeaturedProducts />
     </div>
   );
 };
